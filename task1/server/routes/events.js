c58ef8df9ea82bfbc5df74304fb4c9b619c4200c
Ayush Rudani <ayushcrudani@gmail.com>
2022-06-08 23:05:19 +0530

    Image Upload Resolved
    Ref. for issue and it's solution : https://stackoverflow.com/questions/48418680/enoent-no-such-file-or-directory

--- task1/server/routes/events.js
+++ task1/server/routes/events.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = require('express').Router();
 const dbo = require('../Dao')
-const store = require('../middleware/multer')
 const multer = require('multer');
+
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, 'uploads');
+        cb(null, './uploads/');
     },
     filename: function (req, file, cb) {
-        cb(null, new Date().toISOString() + file.originalname);
+        cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
     }
 });
 
@@ -25,30 +25,31 @@ router.get("/events", async (req, res) => {
     });
 });
 
-router.post("/events", upload.single('eventImages'), async (req, res) => {
-    console.log(req.files);
-    const event = {
-        type: req.body.type,
-        uid: req.body.uid,
-        name: req.body.name,
-        tagline: req.body.tagline,
-        schedule: req.body.schedule,
-        images: [req.files.path],
-        description: req.body.description,
-        moderator: req.body.moderator,
-        category: req.body.category,
-        sub_category: req.body.sub_category,
-        rigor_rank: req.body.rigor_rank,
-        attendees: [...req.body.attendees],
-    }
+router.post("/events", upload.single('eventImage'), async (req, res) => {
+    console.log(req.file);
+    res.send(req.file);
+    // const event = {
+    //     type: req.body.type,
+    //     uid: req.body.uid,
+    //     name: req.body.name,
+    //     tagline: req.body.tagline,
+    //     schedule: req.body.schedule,
+    //     images: [req.files.path],
+    //     description: req.body.description,
+    //     moderator: req.body.moderator,
+    //     category: req.body.category,
+    //     sub_category: req.body.sub_category,
+    //     rigor_rank: req.body.rigor_rank,
+    //     attendees: [...req.body.attendees],
+    // }
 
-    let db_con = dbo.getDb();
+    // let db_con = dbo.getDb();
 
-    db_con.collection("events").insertOne(event, (err, result) => {
-        if (err)
-            throw err;
-        res.json(result);
-    });
+    // db_con.collection("events").insertOne(event, (err, result) => {
+    //     if (err)
+    //         throw err;
+    //     res.json(result);
+    // });
 });
 
 
